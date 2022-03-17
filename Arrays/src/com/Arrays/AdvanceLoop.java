20e229d2e80bfdfbc4df126ea2f46d095723b51c
pradeep sathya <pradeep sathya@LAPTOP-3ACM30J4>
2022-03-17 09:40:12 +0530

    commiting my project

---
+++ Arrays/src/com/Arrays/AdvanceLoop.java
@@ -0,0 +1,10 @@
+package com.Arrays;
+
+public class AdvanceLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Arrays/src/com/Arrays/Number5Series.java
@@ -0,0 +1,20 @@
+package com.Arrays;
+
+import java.util.Scanner;
+
+public class Number5Series {
+	public static void main(String[] args) {
+		int i;
+		int[] arr=new int[5];
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter 5 numbers");
+		for(i=0;i<5;i++) {
+			arr[i]=sc.nextInt();
+		}
+			for(i=0;i<5;i++) {
+				System.out.println(arr[i]);
+}
+}
+	}
+
+
